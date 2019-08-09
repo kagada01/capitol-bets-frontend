a0ae2e967f118e0a3ddb9a6fddc35f1cc79a3d21
Dan Kagan <dankagan7@gmail.com>
2019-08-09 16:16:48 -0400

    added currentUser state to App.js, compDidMount call for User data (incomplete), & updatecurrentUser method, also passed down to Home for a logged in User

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import '../App.css';
 import Navbar from "./Navbar";
 import Login from "./Login";
 import Home from "../containers/Home"
 import MyBets from "../containers/MyBets"
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import store from '../redux/store'
-
 
 class App extends React.Component {
   constructor() {
-    super()
+   super()
 
     this.state = {
-      userLoggedIn: false,
+      currentUser: null,
       allGames: [],
       selectedGame: [],
       allOdds: [],
@@ -21,19 +20,41 @@ class App extends React.Component {
     }
 }
 
+
+  componentDidMount() {
+    fetch('http://localhost:3001/users/1')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+
+//enhanced optic literal
+  updateCurrentUser = (currentUser) => {
+    this.setState({currentUser})
+  }
+
   render() { 
     return (
-      <div>
-      <Navbar />
+     <Fragment>
+        <Navbar />
+      <Switch>
+    
+      <Route exact path="/" render={() => <Redirect to="/home" />} />
+    
       <Route exact path="/login" component={Login}></Route>
-      <Route exact path="/home" component={Home}></Route>
+    
+      <Route exact path="/home" render={()=> 
+        <Home user={this.state.currentUser}/>
+      }/>
+      
       <Route exact path="/mybets" component={MyBets}></Route>
 
-
+      </Switch>
+      </Fragment>
   
-        </div>
     )
   }  
 }
 
-export default App;
+export default withRouter(App);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router";
+import { Button, Form, Segment, Message} from "semantic-ui-react";
 
 
 class Login extends React.Component {
@@ -7,8 +9,45 @@ class Login extends React.Component {
     render() {
         return (
 
-            <div className="">
-            Login</div>
+            // <div className="">
+            // Login
+            
+            
+            
+            // </div>
+
+            <Segment>
+        <Form
+          onSubmit={this.handleLoginSubmit}
+          size="mini"
+          key="mini"
+          loading={this.props.authenticatingUser}
+          error={this.props.failedLogin}
+        >
+          <Message
+            error
+            header={this.props.failedLogin ? this.props.error : null}
+          />
+          <Form.Group widths="equal">
+            <Form.Input
+              label="username"
+              placeholder="username"
+              name="username"
+            //   onChange={this.handleChange}
+            //   value={this.state.username}
+            />
+            <Form.Input
+              type="password"
+              label="password"
+              placeholder="password"
+              name="password"
+            //   onChange={this.handleChange}
+            //   value={this.state.password}
+            />
+          </Form.Group>
+          <Button type="submit">Login</Button>
+        </Form>
+      </Segment>
         )
     }
     
