2b242d6dc2dd3913e6c8e1244f6656650f144ffc
Dan Kagan <dankagan7@gmail.com>
2019-08-14 13:21:23 -0400

    iterated through and displayed full Upcoming Game data on homepage, but ran into SportsRadarAPI issue midway through

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,22 +1,40 @@
 import React, { Component, Fragment } from "react"
+import { Card } from "semantic-ui-react"
 import BetCard from "./BetCard"
 
-const GameCard = (props) => {
+const src = '/images/wireframe/white-image.png'
 
-        return (
-            <Fragment>
+const CardExampleLinkCardProp = (props) => (
+    <Card
+      link
+      header={`${props.gameObj.game_team2} @ ${props.gameObj.game_team1}`}
+      meta='MLB'
+      description={[  `${props.gameObj.game_venue}, ${props.gameObj.game_location}`
+    //   
+      .join('')]}
+    />
+  )
+  
+  export default CardExampleLinkCardProp
+  
+
+
+// const GameCard = (props) => {
+
+//         return (
+//             <Fragment>
             
 
-                <h4>Home: {props.gameObj.game_team1} | Away: {props.gameObj.game_team2} </h4>
-                <p>Location: {props.gameObj.game_venue} | {props.gameObj.game_location} </p>
+//                 <h4>Home: {props.gameObj.game_team1} | Away: {props.gameObj.game_team2} </h4>
+//                 <p>Location: {props.gameObj.game_venue} | {props.gameObj.game_location} </p>
 
 
-            <BetCard />
+
             
-            </Fragment>
-        )
+//             </Fragment>
+//         )
 
 
-}
+// }
 
-export default GameCard;
+// export default GameCard;
--- src/containers/GamesContainer.js
+++ src/containers/GamesContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import GameCard from "../components/GameCard"
+var moment = require('moment');
 
 const GamesContainer = (props) => {
 
     return (
             <div className="GamesContainer">   
-               <h2>Upcoming Games</h2>
+               <h3>Upcoming Games: {moment().format('MMMM Do, YYYY')}</h3>
                 {
                 props.allGames.map(gameObj => <GameCard 
                     key={gameObj.id}
